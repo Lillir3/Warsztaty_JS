2b1e61f5837bdc8ce342e21c09d725fcba0c5046
Lillir3 <barbara.grono@gmail.com>
2018-09-29 12:51:43 +0200

    zadanie nr 1

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+      
+    var menuButton = document.querySelector('.for-dropdown');
+     console.log (menuButton);
+    var submenu= document.querySelector('.dropdown');
+    
+    
+    menuButton.addEventListener('mouseover', function() {
+        submenu.style.display = 'block';
+        
+    });
+    
+    menuButton.addEventListener('mouseout', function() {
+        submenu.style.display = 'none';
+    });
+    
+     });
+
+
+
